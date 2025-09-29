11c3a70e86d9bea313b44aeb9a114cc0da2a15c6
Hete Caetano <hetecaetano@gmail.com>
2025-09-29 14:49:23 -0300

    Criado os formularios de paciente e listar paciente

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { FormularioPaciente } from './components/FormularioPaciente';
+import { ListaPacientes } from './components/ListaPacientes';
+import './styles.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [pacientes, setPacientes] = useState([]);
+  const [jaCarregou, setJaCarregou] = useState(false);
+
+  useEffect(() => {
+    const dadosSalvos = localStorage.getItem('pacientes');
+    if (dadosSalvos) setPacientes(JSON.parse(dadosSalvos));
+    setJaCarregou(true);
+  }, []);
+
+  useEffect(() => {
+    // Só salva no localStorage após ter carregado os dados iniciais
+    if (jaCarregou) {
+      localStorage.setItem('pacientes', JSON.stringify(pacientes));
+    }
+  }, [pacientes, jaCarregou]);
+
+  function adicionarPaciente(p) {
+    setPacientes([...pacientes, p]);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>CardioIA – Batimentos de Dados</h1>
+      <FormularioPaciente onAddPaciente={adicionarPaciente} />
+      <ListaPacientes pacientes={pacientes} />
+    </div>
+  );
 }
-
-export default App
